68ed0b62619b37246effb88dba0869c0e044bdb4
Thomas Mattsson <32563069+Tmattsson0@users.noreply.github.com>
2017-11-07 11:58:18 +0100

    Added TwoDice to Main

--- src/diceGame/TwoDice.java
+++ src/diceGame/TwoDice.java
@@ -2,24 +2,24 @@ package diceGame;
 
 public class TwoDice {
 
-	private int die1; //value of 1st die.
-	private int die2; //value of 2nd die.
+	private static int die1;	//value of 1st die.
+	private static int die2;	//value of 2nd die.
 
 	public TwoDice() {
 		roll();
 	}
-	public void roll() {
+	public static void roll() {		//Rolls both dice to a value between 1 and 6.
 
 		die1 = (int)(Math.random())*6 + 1;
 		die2 = (int)(Math.random())*6 + 1;
 	} 
-	public int getdie1() {
+	public int getdie1() {	//Returns value of die 1.
 		return die1;
 	}
-	public int getdie2() {
+	public int getdie2() {	//Returns value of die 2.
 		return die2;
 	}
-	public int getDiceTotal() {
+	public int getDiceTotal() {	//Returns the values of the two dice added together.
 		return die1 + die2;
 	}
 }
