fcf92f21173c581c9cd85706d54c067fd97a8ac8
rallerG <raller799@live.dk>
2017-11-07 16:55:46 +0100

    Loads of shit code, that is under halfway done

--- src/diceGame/AccountBalance.java
+++ src/diceGame/AccountBalance.java
@@ -1,5 +1,29 @@
 package diceGame;
 
 public class AccountBalance {
+	private int balance;
+
+	public AccountBalance(int balance) {
+		super();
+		this.balance = balance;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		if (this.balance < balance) {
+			this.balance = 0;
+		} else {
+		this.balance = balance;
+		}
+	}
+	
+//	public static void updateBalance(Player name, int gold) {
+//		name.balance += gold;
+//	}
+
+
 
 }
