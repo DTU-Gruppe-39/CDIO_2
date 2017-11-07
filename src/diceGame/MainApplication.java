cdb95fa34ee130f201ad5b6dc5d9ec1bab9e6ff5
rallerG <raller799@live.dk>
2017-11-07 12:04:10 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/diceGame/MainApplication.java

--- src/diceGame/MainApplication.java
+++ src/diceGame/MainApplication.java
@@ -4,7 +4,13 @@ public class MainApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test");
-	}
+		TwoDice dice;
 
+		dice = new TwoDice();
+
+		TwoDice.roll();
+		System.out.println("The dice come up " + dice.getdie1() 
+		+ " and " + dice.getdie2());
+		System.out.println("The total is: " + dice.getDiceTotal());
+	}
 }
