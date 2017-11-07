baab2ed1d1643465a1d75b800865ff26a7a80c3d
Thomas Mattsson <32563069+Tmattsson0@users.noreply.github.com>
2017-11-07 13:05:10 +0100

    Added comments in TwoDice.

--- src/diceGame/TwoDice.java
+++ src/diceGame/TwoDice.java
@@ -5,18 +5,19 @@ public class TwoDice {
 	private static int die1;	//value of 1st die.
 	private static int die2;	//value of 2nd die.
 	private static int faces = 6;
+	
 	public TwoDice() {
 		roll();
 	}
 	public static void roll() {		//Rolls both dice to a value between 1 and 6.
 
-		double d1=(float)Math.random();     // [0 ; 1[
-		double d2 = d1*faces;
-		die1 = (int)Math.ceil(d2);
-		
-		double d3=(float)Math.random();     // [0 ; 1[
-		double d4 = d3*faces;
-		die2 = (int)Math.ceil(d4);
+		double d1=(float)Math.random();		// [0 ; 1[
+		double d2 = d1*faces;				// [0 ; 6[
+		die1 = (int)Math.ceil(d2);			// [1-6] integer
+
+		double d3=(float)Math.random();		// [0 ; 1[
+		double d4 = d3*faces;				// [0 ; 6[
+		die2 = (int)Math.ceil(d4);			// [1-6] integer
 
 	} 
 	public int getdie1() {	//Returns value of die 1.
