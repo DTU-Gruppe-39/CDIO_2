db413d5fadc5565920f5a20a07440c7070576d8e
Thomas Mattsson <32563069+Tmattsson0@users.noreply.github.com>
2017-11-07 12:57:54 +0100

    Changed "Two Dice" randomize method
    Reverted back to the CDIO_1 way of making random numbers.

--- src/diceGame/TwoDice.java
+++ src/diceGame/TwoDice.java
@@ -1,18 +1,23 @@
 package diceGame;
-import java.util.Random;
 
 public class TwoDice {
 
 	private static int die1;	//value of 1st die.
 	private static int die2;	//value of 2nd die.
-
+	private static int faces = 6;
 	public TwoDice() {
 		roll();
 	}
 	public static void roll() {		//Rolls both dice to a value between 1 and 6.
 
-		die1 = (int)(Math.random()*6) + 1;
-		die2 = (int)(Math.random()*6) + 1;
+		double d1=(float)Math.random();     // [0 ; 1[
+		double d2 = d1*faces;
+		die1 = (int)Math.ceil(d2);
+		
+		double d3=(float)Math.random();     // [0 ; 1[
+		double d4 = d3*faces;
+		die2 = (int)Math.ceil(d4);
+
 	} 
 	public int getdie1() {	//Returns value of die 1.
 		return die1;
