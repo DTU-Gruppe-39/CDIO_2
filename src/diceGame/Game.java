d7d004a37bbe1d309fa5e6db770b43310d44e4dc
Thomas Mattsson <32563069+Tmattsson0@users.noreply.github.com>
2017-11-09 11:45:25 +0100

    Created dice outside main.

--- src/diceGame/Game.java
+++ src/diceGame/Game.java
@@ -13,9 +13,7 @@ import gui.Test;
 public class Game {
 	private static Player player1;
 	private static Player player2;
-	
-	
-//	private TwoDice dice = new TwoDice(1,1);
+	private static TwoDice dice = new TwoDice(1,1);
 
 	public static void createPlayers(String PlayerOneName, String PlayerTwoName) {
 		player1 = new Player(PlayerOneName, 1, 1, 1000);
@@ -28,7 +26,7 @@ public class Game {
 		Test.GUILauncher();
 //		while (Player one.balance < 3000 && Player two.balance < 3000) {
 		
-		TwoDice dice = new TwoDice(1, 1);
+//		TwoDice(1, 1);
 
 		if (whosTurn == 0) {
 			Game Turn = new Game();
@@ -95,6 +93,14 @@ public class Game {
 
 
 	
+	private static void TwoDice(int i, int j) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
 	//Read from textFile
 	public static void printText (int field) throws IOException {
 		String str = "";
