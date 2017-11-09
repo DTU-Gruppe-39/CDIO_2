4fd726326997dcf018c25398768e30c33640bf9f
Patrick Marcell Madsen <32563752+DtuPat@users.noreply.github.com>
2017-11-09 12:30:07 +0100

    Tested AccountBalance more in J_unit test
    Tested if it functioned properly and if it could handle Integer max and min to be the balance

--- src/test/AccountBalanceNegativBalanceTest.java
+++ src/test/AccountBalanceNegativBalanceTest.java
@@ -27,4 +27,49 @@ public class AccountBalanceNegativBalanceTest {
 		Assert.assertEquals(Expected,Actual);
 	}
 
+	@Test
+	public void test2() {
+		ac.setBalance(-200);
+		int Expected = 800;
+		int Actual = ac.getBalance();
+		Assert.assertEquals(Expected,Actual);
+	}
+	@Test
+	public void test3() {
+		ac.setBalance(-1000);
+		int Expected = 0;
+		int Actual = ac.getBalance();
+		Assert.assertEquals(Expected,Actual);
+	}
+	@Test
+	public void test4() {
+		ac.setBalance(1000);
+		int Expected = 2000;
+		int Actual = ac.getBalance();
+		Assert.assertEquals(Expected,Actual);
+	}
+	@Test
+	public void test5() {
+		ac.setBalance(-0);
+		int Expected = 1000;
+		int Actual = ac.getBalance();
+		Assert.assertEquals(Expected,Actual);
+	}
+	@Test
+	public void test6() {
+		ac.setBalance(Integer.MIN_VALUE);
+		int Expected = 0;
+		int Actual = ac.getBalance();
+		Assert.assertEquals(Expected,Actual);
+	}
+	@Test
+	public void test7() {
+		ac.setBalance(-1000);
+		ac.setBalance(Integer.MAX_VALUE);
+		int Expected = Integer.MAX_VALUE;
+		int Actual = ac.getBalance();
+		Assert.assertEquals(Expected,Actual);
+	}
+	
+	
 }
